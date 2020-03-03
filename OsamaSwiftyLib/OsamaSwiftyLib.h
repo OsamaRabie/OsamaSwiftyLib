63dea8342f84075266d2dad6a151b710b3dcb9bf
OsamaRabie <o.rabie@tap.company>
2020-03-03 16:44:21 +0200

    Initial Commit

---
+++ OsamaSwiftyLib/OsamaSwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  OsamaSwiftyLib.h
+//  OsamaSwiftyLib
+//
+//  Created by Osama Rabie on 03/03/2020.
+//  Copyright © 2020 Osama Rabie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OsamaSwiftyLib.
+FOUNDATION_EXPORT double OsamaSwiftyLibVersionNumber;
+
+//! Project version string for OsamaSwiftyLib.
+FOUNDATION_EXPORT const unsigned char OsamaSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OsamaSwiftyLib/PublicHeader.h>
+
+
