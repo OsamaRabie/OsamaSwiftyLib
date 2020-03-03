3625d5d9b9a40be9ce61311d35e32384f697a836
OsamaRabie <o.rabie@tap.company>
2020-03-03 16:47:11 +0200

    Added OsamaMath Class

---
+++ OsamaSwiftyLib/Source/OsamaMath.swift
@@ -0,0 +1,25 @@
+//
+//  OsamaMath.swift
+//  OsamaSwiftyLib
+//
+//  Created by Osama Rabie on 03/03/2020.
+//  Copyright © 2020 Osama Rabie. All rights reserved.
+//
+
+import Foundation
+
+
+
+public final class OsamaMath {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ OsamaSwiftyLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OsamaSwiftyLibExample
+//
+//  Created by Osama Rabie on 03/03/2020.
+//  Copyright © 2020 Osama Rabie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
